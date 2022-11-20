458eaf3ff526bc6a6a52e7665058355a3abdb33e
Steven <steven.camillieri@protonmail.com>
2022-11-19 20:50:50 -0500

    CRUD and auth/jwt

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,11 @@
 const asyncHandler = require("express-async-handler");
 
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
-  res.status(200).json({ goals });
+  const goals = await Goal.find({ user: req.user.id });
+  res.status(200).json({ goals, name: req.user.name });
 });
 
 const setGoal = asyncHandler(async (req, res) => {
@@ -15,6 +16,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json({ goal });
@@ -22,28 +24,54 @@ const setGoal = asyncHandler(async (req, res) => {
 
 const updateGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
+
   if (!goal) {
     res.status(400);
     throw new Error("goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("user not found");
+  }
+
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
 
-  res.status(200).json(updateGoal);
+  res.status(200).json(updatedGoal);
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
+
   if (!goal) {
     res.status(400);
     throw new Error("no goal to delete!");
   }
 
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("user not found");
+  }
+
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
   await goal.remove();
 
-  res.status(200).json({ id: req.params.id });
+  res.status(200).json({ message: "successfully deleted", id: req.params.id });
 });
 
 module.exports = {
@@ -52,3 +80,5 @@ module.exports = {
   updateGoal,
   deleteGoal,
 };
+
+// failing deleteGoal
